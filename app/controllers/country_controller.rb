ba4ad9ab4ad3bf73392a1548c66fcd321ca6e971
Ling Jin <chloejin525@gmail.com>
2017-07-05 08:33:33 -0400

    partial used

--- app/controllers/country_controller.rb
+++ app/controllers/country_controller.rb
@@ -1,10 +1,17 @@
 class CountryController < ApplicationController
   def all_states
+  	@states = State.all
   end
 
   def top_five_pop
+  	@states = State.all.sort_by {|state| state.population}
+	@states.reverse!
+	
   end
 
   def top_five_area
+  	@states = State.all.sort_by {|state| state.area}
+	@states.reverse!
+	
   end
 end
